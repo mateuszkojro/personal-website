74aac8b9b2a693d0642e78fa75c278e6e694fd07
Mateusz Kojro <mateuszkojro@outlook.com>
2021-07-08 13:19:04 +0200

    Adding Semantic-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button, Header } from "semantic-ui-react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <Head>Home page</Head>;
+  return (
+    <div>
+      <Header textAlign="center" as="h1">Home page</Header>
+    </div>
+  );
 }
