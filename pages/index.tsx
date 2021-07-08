25db713ee213532bcd6c0f07aea242adedc5431d
Mateusz Kojro <mateuszkojro@outlook.com>
2021-07-08 14:57:47 +0200

    Home site template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,175 @@
-import { Button, Header } from "semantic-ui-react";
+import {
+  Button,
+  Container,
+  Divider,
+  Dropdown,
+  Grid,
+  Header,
+  Icon,
+  List,
+  Menu,
+  Segment,
+} from "semantic-ui-react";
+
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import React from "react";
+
+const FixedMenu = () => {
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item as="a" header>
+          {/* <Image size="mini" src="/logo.png" style={{ marginRight: "1.5em" }} /> */}
+          Mateusz Kojro
+        </Menu.Item>
+        <Menu.Item as="a">Home</Menu.Item>
+        <Menu.Item as="a">Github</Menu.Item>
+        <Menu.Item as="a">About me</Menu.Item>
+
+        <Dropdown item simple text="Dropdown">
+          <Dropdown.Menu>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Header Item</Dropdown.Header>
+            <Dropdown.Item>
+              <i className="dropdown icon" />
+              <span className="text">Submenu</span>
+              <Dropdown.Menu>
+                <Dropdown.Item>List Item</Dropdown.Item>
+                <Dropdown.Item>List Item</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown.Item>
+            <Dropdown.Item>List Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Container>
+    </Menu>
+  );
+};
 
 export default function Home() {
   return (
     <div>
-      <Header textAlign="center" as="h1">Home page</Header>
+      <FixedMenu />
+      <Segment style={{ padding: "8em 0em" }} vertical>
+        <Grid container stackable verticalAlign="middle">
+          <Grid.Row>
+            <Grid.Column width={8}>
+              <Header as="h1" style={{ fontSize: "5em" }}>
+                Hi!
+              </Header>
+              <p style={{ fontSize: "1.33em" }}>
+                My name is Mateusz, and I am studying Physics and Computer
+                Science at Univeristy of Lodz in Poland
+              </p>
+            </Grid.Column>
+            <Grid.Column floated="right" width={6}>
+              {/* <Image bordered rounded size='large' src='/images/wireframe/white-image.png' /> */}
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column textAlign="center">
+              <Button icon labelPosition="left" size="huge">
+                <Icon name="file" />
+                My CV
+              </Button>
+              <Button
+                icon
+                labelPosition="left"
+                size="huge"
+                as="a"
+                href="https://github.com/mateuszkojro"
+              >
+                <Icon name="github square" />
+                Check out my code!
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment style={{ padding: "0em" }} vertical>
+        <Grid celled="internally" columns="equal" stackable>
+          <Grid.Row textAlign="center">
+            <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+              <Header as="h3" style={{ fontSize: "2em" }}>
+                "Some text"
+              </Header>
+              <p style={{ fontSize: "1.33em" }}>subtext</p>
+            </Grid.Column>
+            <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+              <Header as="h3" style={{ fontSize: "2em" }}>
+                "Some text"
+              </Header>
+              <p style={{ fontSize: "1.33em" }}>
+                {/* <Image avatar src='/images/avatar/large/nan.jpg' /> */}
+                subtext
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment style={{ padding: "8em 0em" }} vertical>
+        <Container text>
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            Section 1
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>some info</p>
+          <Button as="a" size="large">
+            Read More
+          </Button>
+
+          <Divider
+            as="h4"
+            className="header"
+            horizontal
+            style={{ margin: "3em 0em", textTransform: "uppercase" }}
+          >
+            <a href="#">Case Studies</a>
+          </Divider>
+
+          <Header as="h3" style={{ fontSize: "2em" }}>
+            Section 2
+          </Header>
+          <p style={{ fontSize: "1.33em" }}>quick info</p>
+          <Button as="a" size="large">
+            More info
+          </Button>
+        </Container>
+      </Segment>
+
+      <Segment inverted vertical style={{ padding: "5em 0em" }}>
+        <Container>
+          <Grid divided inverted stackable>
+            <Grid.Row>
+              <Grid.Column width={7}>
+                <Header as="h4" inverted>
+                  Some links:
+                </Header>
+                <p>
+                  Something quick
+                </p>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="About" />
+                <List link inverted>
+                  <List.Item as="a">Sitemap</List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Header inverted as="h4" content="Services" />
+                <List link inverted>
+                  <List.Item as="a">Banana Pre-Order</List.Item>
+                </List>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Segment>
     </div>
   );
 }
