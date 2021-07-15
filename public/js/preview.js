2ff4fc1cb13c86f705c901dca85187b087e50b71
Mateusz Kojro <mateuszkojro@outlook.com>
2021-07-15 23:50:56 +0200

    Adding a simple way of rendering notebooks

---
+++ public/js/preview.js
@@ -0,0 +1,32 @@
+(function () {
+  var root = this;
+  var $file_input = document.querySelector("input#file");
+  var $holder = document.querySelector("#notebook-holder");
+
+  document.q;
+
+  var render_notebook = function (ipynb) {
+    var notebook = (root.notebook = nb.parse(ipynb));
+    while ($holder.hasChildNodes()) {
+      $holder.removeChild($holder.lastChild);
+    }
+    $holder.appendChild(notebook.render());
+    Prism.highlightAll();
+  };
+
+  const params = new URLSearchParams(location.search);
+  console.log(params);
+  fetch(params.get("notebook"))
+    .then((res) => {
+      // console.log("result", res.body.json());
+      return res.json();
+    })
+    .then((text) => {
+      console.log("Text: ", text);
+      // try {
+        render_notebook(text);
+      // } finally {
+      //   $holder.innerHTML = "Notebook not found";
+      // }
+    });
+}.call(this));
